1205dc3da3a894347e6e7fe471d1763beed40042
aroharia <aroharia@gmail.com>
2017-10-17 19:01:27 -0500

    added fetch.py
    script that uses the Facebook API to fetch data from posts on Facebook
    Pages

---
+++ fetch.py
@@ -0,0 +1,9 @@
+# fetch.py
+# EE 461L - Software Engineering & Design Lab
+# --------------------------------------------
+# Use the Facebook API to fetch data from posts on Facebook Pages
+# INPUT: ID of Facebook page
+# OUTPUT: CSV file of the Facebook page's post data
+
+if __name__ == '__main__':
+    print ("Facebook Page Data")
